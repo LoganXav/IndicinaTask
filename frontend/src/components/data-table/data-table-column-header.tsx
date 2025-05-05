3504b9eb8698e7d15c24e46cd1347c87cd251d3e
Segun <sogbesansegun22@gmail.com>
2025-05-05 11:09:11 +0100

    Feat(docker): dockerize the app and add start commands

--- frontend/src/components/data-table/data-table-column-header.tsx
+++ frontend/src/components/data-table/data-table-column-header.tsx
@@ -1,7 +1,8 @@
-import * as React from "react";
-import { CaretSortIcon } from "@radix-ui/react-icons";
-import { Column } from "@tanstack/react-table";
-import { Button, cn } from "@repo/ui";
+import * as React from 'react';
+import { CaretSortIcon } from '@radix-ui/react-icons';
+import { Column } from '@tanstack/react-table';
+import { Button } from '~/components/globals/button';
+import { cn } from '~/utils';
 
 interface DataTableColumnHeaderProps<TData, TValue>
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -15,11 +16,10 @@ export function DataTableColumnHeader<TData, TValue>({
   title,
 }: DataTableColumnHeaderProps<TData, TValue>) {
   return (
-    <div className={cn("flex items-center space-x-2", className)}>
+    <div className={cn('flex items-center space-x-2', className)}>
       <Button
         variant="ghost"
-        size="page"
-        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         className="flex items-center space-x-1 p-0 text-primary"
       >
         <span className="text-base font-heading">{title.toUpperCase()}</span>
